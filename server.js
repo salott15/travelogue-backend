7643404e5f239ab94bbe128b65229e1c2fa53fb5
salott15 <sarilott@yahoo.com>
2018-05-08 20:14:18 -0700

    undo bug fix

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static(__dirname + '/public'));
 // CORS
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
     if (req.method === 'OPTIONS') {
         return res.sendStatus(204);
