cb9337bca31fff254da024eaa2e0321f4b97ad00
salott15 <sarilott@yahoo.com>
2018-06-04 09:52:45 -0700

    bugs

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     if (req.method === 'OPTIONS') {
         return res.sendStatus(204);
     }
